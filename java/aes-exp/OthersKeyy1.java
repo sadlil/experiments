714bf5f7862ccd6a141d117b696d7286eb75e061
sadlil <sadlil@appscode.com>
2016-12-06 17:06:35 +0600

    reduced loops.

--- java/aes-exp/OthersKeyy1.java
+++ java/aes-exp/OthersKeyy1.java
@@ -8,9 +8,7 @@ public class OthersKeyy1 {
 		}
 
 		for (int i=0; i<matrix2.length; i++) {
-			for (int j=0; j<matrix2[i].length; j++) {
-				System.out.print(matrix3[i][j]);
-			}
+			System.out.print(matrix3[i][0]);
 			System.out.println();
 		}
 	}
