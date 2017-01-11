159d5bb33936c7964a1ca32247b4befefeaf75ce
sadlil <sadlil@appscode.com>
2017-01-11 23:36:49 +0600

    stop multiple goroutine togather

---
+++ go/multiroutine/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func goRoutine(num int, stop chan struct{}, writechan chan int) {
+	for i := 1; i <= num; i++ {
+		select {
+		case <-stop:
+			fmt.Println("stopping", num)
+			return
+		default:
+		}
+		fmt.Println("running", num)
+		time.Sleep(time.Second * 5)
+	}
+	writechan <- num
+}
+
+func main() {
+	stop := make(chan struct{})
+	write := make(chan  int)
+	for i := 1; i <= 10; i++ {
+		go goRoutine(i, stop, write)
+	}
+
+	res, ok := <-write
+	if ok {
+		fmt.Println("Finishes", res)
+		close(stop)
+		time.Sleep(time.Second*2)
+	}
+}
