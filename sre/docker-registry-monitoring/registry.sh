514ea1ae075d644e9a6758411297850135ee2563
Sadlil <sadlil.rhythm@gmail.com>
2019-07-31 16:51:58 +0200

    add a bash script to run and check private docker registry

---
+++ sre/docker-registry-monitoring/registry.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -e
+
+start() {
+    docker run -d -p 5000:5000 --restart=always --name registry registry:2
+}
+
+stop() {
+    docker container stop registry
+    docker container rm -v registry
+}
+
+check() {
+    docker pull alpine:latest
+    docker tag alpine:latest localhost:5000/alpine:test
+    docker push localhost:5000/alpine:test
+
+    docker image remove alpine:latest
+    docker image remove localhost:5000/alpine:test
+
+    docker pull localhost:5000/alpine:test
+
+    docker image remove localhost:5000/alpine:test
+}
+
+RETVAL=0
+if [ $# -eq 0 ]; then
+  echo "No Target specified"
+  exit 1
+fi
+
+case "$1" in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  check)
+    check
+    ;;
+  *)
+    (10)
+    echo $"Usage: $0 {up|down}"
+    RETVAL=1
+    ;;
+esac
+
+exit ${RETVAL}
